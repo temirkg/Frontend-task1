bece147d57fb87c1d84e275109d940b25a413d61
temirkg <ariurimen@gmail.com>
2022-01-25 00:29:38 +0600

    dz28

---
+++ Урок 28/add_to_cart.js
@@ -0,0 +1,7 @@
+let ttl = document.getElementsByClassName("ttl"); 
+console.log(ttl[1].innerText);
+
+let price = document.getElementsByClassName('price');
+console.log(price[0].innerText);
+let images1 = document.getElementsByClassName('images1');
+console.log(images1[0]);
