1bbbc6b993474b7430965efdc21e5ee07b9eb2f6
temirkg <ariurimen@gmail.com>
2022-03-19 00:44:07 +0600

    43

---
+++ Урок 43/script.js
@@ -0,0 +1,19 @@
+
+const click1 = document.querySelector("#a");
+
+click1.addEventListener('click', function(){
+    document.body.style.background = "rebeccapurple";
+});
+
+const click2 = document.querySelector("#b");
+
+click2.addEventListener('click', function(){
+    document.body.style.background = "black";
+});
+
+const click3 = document.querySelector("#c");
+
+click3.addEventListener('click', function(){
+    document.body.style.background = "blue";
+});
+
